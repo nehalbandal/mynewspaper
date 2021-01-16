652ae8c416de0040a09977c3528540fce89fdc02
nehalbandal <nihalbandal9@gmail.com>
2021-01-16 17:39:18 +0530

    Updated

--- MyNewspaper/settings.py
+++ MyNewspaper/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "mynewspaper84.herokuapp.com",]
+ALLOWED_HOSTS = ["127.0.0.1", "mynewspaperapp.herokuapp.com",]
 
 
 # Application definition
