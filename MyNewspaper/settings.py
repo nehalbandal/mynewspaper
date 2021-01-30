c32b360ec5680a157848146f8e42c1fbf36f3bec
nehalbandal <nihalbandal9@gmail.com>
2021-01-30 13:01:36 +0530

    Updated settings

--- MyNewspaper/settings.py
+++ MyNewspaper/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,4 +132,4 @@ LOGIN_URL = '/users/login/'
 LOGOUT_REDIRECT_URL = '/users/login/'
 LOGIN_REDIRECT_URL = '/'
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
