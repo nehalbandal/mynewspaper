02536723ec778c77efaeee47db881562a537125f
nehalbandal <nihalbandal9@gmail.com>
2021-01-30 14:23:56 +0530

    updated settings

--- MyNewspaper/settings.py
+++ MyNewspaper/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get("DEBUG_VALUE") == "True")
 
-ALLOWED_HOSTS = ["127.0.0.1", "mynewspaperapp.herokuapp.com", "nehalbandal84.pythonanywhere.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "mynewspaperapp.herokuapp.com"]
 
 
 # Application definition
