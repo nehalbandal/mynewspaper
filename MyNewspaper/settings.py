b700330df187fbc57a0094867eef83becaca57f4
nehalbandal <nihalbandal9@gmail.com>
2021-01-30 13:09:09 +0530

    Updated settings

--- MyNewspaper/settings.py
+++ MyNewspaper/settings.py
@@ -25,7 +25,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get("DEBUG_VALUE") == "True")
 
 ALLOWED_HOSTS = ["127.0.0.1", "mynewspaperapp.herokuapp.com", "nehalbandal84.pythonanywhere.com"]
 
